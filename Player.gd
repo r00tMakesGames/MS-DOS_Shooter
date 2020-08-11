0c007e7420df83c34bbb4bd82de3bd8bac55007d
r00tMakesGames <adhyanmall@gmail.com>
2020-08-11 23:23:38 +0530

    sound and more enemies

--- Player.gd
+++ Player.gd
@@ -37,6 +37,7 @@ func _physics_process(delta):
  
 	if Input.is_action_pressed("shoot") and !anim_player.is_playing():
 		anim_player.play("shootanimation")
+		$sound.play()
 		var coll = raycast.get_collider()
 		if raycast.is_colliding() and coll.has_method("kill"):
 			coll.kill()
